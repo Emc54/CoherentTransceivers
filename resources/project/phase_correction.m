f6fe8cef9cec684ba6421fc1f0ee2145ee781d4b
Emc54 <35582806+Emc54@users.noreply.github.com>
2021-03-14 03:12:12 +0000

    Started implementation on SSFM, modified phase correction to work in blocks

---
+++ resources/project/phase_correction.m
@@ -0,0 +1,37 @@
+function phase_corrected_signal = phase_correction(input_signal,chunk_length)
+%PHASE_CORRECTION Tries to estimate and correct phase on qpsk
+%   Detailed explanation goes here
+
+
+
+%Attempt to find the mean angle in chunks
+raised_phase_signal = input_signal.^4;
+signal_length = length(input_signal);
+chunk_size = [chunk_length,1];
+
+raised_phase_signal = filtfilt(ones(100,1)/100,1,raised_phase_signal);
+
+if size(raised_phase_signal,2) > size(raised_phase_signal,1)
+    raised_phase_signal = raised_phase_signal.';
+end
+
+mean_filter_fun = @(theBlockStructure) mean2(theBlockStructure.data(:));
+averages = blockproc(raised_phase_signal,chunk_size,mean_filter_fun);
+phase_estimates = 1/4.*angle(averages);
+
+multiple = signal_length - mod(signal_length,chunk_length);
+reshaped = reshape(input_signal(1:multiple),chunk_length,[]);
+leftover = input_signal(multiple+1:end);
+leftover = padarray(leftover,[chunk_length-length(leftover) 0],0,'post');
+
+combined = [reshaped leftover];
+
+for iter=1:size(reshaped,2)
+    combined(:,iter) = combined(:,iter).*exp(1j*pi/4-1j*phase_estimates(iter)+3j*pi/2);
+end
+
+phase_compensated = reshape(combined,[],1).';
+
+phase_corrected_signal = phase_compensated(1:signal_length);
+end
+
